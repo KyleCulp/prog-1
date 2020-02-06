f877f783ea71c3dae1c9be647c73359aea4c480f
Kyle <me@kyleculp.com>
2020-02-06 16:45:08 -0600

    Create HousePaymentPartOne.cpp

---
+++ HousePaymentPartOne.cpp
@@ -0,0 +1,57 @@
+// Kyle Culp
+// Programming 1
+// Due: 2/13/2020
+// House Payment Part 1
+// Get the monthly payment cost for a loan by providing
+// the principal, yearly interest rate, and the loan length
+
+#include <iostream>
+#include <iomanip> 
+#include <math.h> 
+using namespace std; 
+
+/* 
+* mp: (p * mir) / 1 - (1 / 1 + mir) ^ (12 * y)
+* tophalf: (p * mir)
+* bottomhalf: 1 - (1 / 1 + mir) ^ (12 * y)
+* mp: monthly payment
+* p: principal
+* mir: interest rate for 1 month
+* y: year
+* yir: interest rate for 1 year
+*/ 
+
+double calculateMonthlyPayment(double principal, double yir, double years) {
+  // Get monthly interest rate 
+  double mir = yir / 1200;
+
+  // Exponent to send to pow function 
+  double exponent = 12 * years;
+
+  double bottomHalf = mir + 1;
+  bottomHalf = 1 / bottomHalf;
+  bottomHalf = pow(bottomHalf, exponent);
+  bottomHalf = 1 - bottomHalf;
+  double topHalf = principal * mir;
+
+  return topHalf / bottomHalf;
+}
+
+int main() {
+  double principal, yir, years;
+  cout << setprecision(2) << fixed;
+
+  cout << "Enter the principal: ";
+  cin >> principal;
+  cout << "Enter the yearly interest rate (in decimal format): ";
+  cin >> yir;
+  cout << "Enter the loan length (in years): ";
+  cin >> years;
+
+  double monthlyPayment = calculateMonthlyPayment(principal, yir, years);
+
+  cout << "Monthly Payment: $" << monthlyPayment << endl;
+
+  return 0;
+}
+
