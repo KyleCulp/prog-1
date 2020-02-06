afd75c6436072dd4fda9266b4c40f28e1e2cb17c
Kyle <me@kyleculp.com>
2020-02-06 10:43:58 -0600

    Create BankCodes.cpp

---
+++ BankCodes.cpp
@@ -0,0 +1,20 @@
+
+// Kyle Culp
+// Programming 1
+// Due: 2/6/2020
+// Bank Codes
+// Enter a bank code to verify if it's valid
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  string bankCode;
+  cout << "Enter the bank code: ";
+  cin >> bankCode;
+
+  
+
+
+  return 0;
+}
